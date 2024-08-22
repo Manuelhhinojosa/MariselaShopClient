bf6daeebb963cdf56b57cb9cb23f270867db0d7d
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2024-08-22 11:59:23 -0400

    Altered various elements' syle based on client's request

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Routes, Route } from "react-router-dom";
 // components
 import { Home } from "./components/pageComponents/Home/Home";
 import { Navbar } from "./components/generalComoponents/Navbar/Navbar";
+import { Footer } from "./components/generalComoponents/Footer/Footer";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-      <div className="footer"></div>
+      <Footer />
     </div>
   );
 }
