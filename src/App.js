faed81cfb81bd660d90a52f3741782484568accb
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2024-08-22 13:24:08 -0400

    Added ErrorPage component to handle nonexisting routes/urls

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./components/pageComponents/Home/Home";
 import { Navbar } from "./components/generalComoponents/Navbar/Navbar";
 import { Footer } from "./components/generalComoponents/Footer/Footer";
+import { ErrorPage } from "./components/pageComponents/ErrorPage/ErrorPage";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
       <Footer />
     </div>
