2775ad49bfd75d3fc2d6312eb2d896a569be2b89
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2024-08-26 11:57:58 -0400

    Created Prod component. This comp. contains the product information and will be used dynamically inside of Shop cmponent

---
+++ src/components/generalComoponents/Prod/Prod.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./Prod.scss";
+
+export const Prod = () => {
+  return <div className="prodContainer"></div>;
+};
--- src/components/generalComoponents/ShopContainer/ShopContainer.jsx
+++ src/components/generalComoponents/ShopContainer/ShopContainer.jsx
@@ -3,6 +3,13 @@ import React from "react";
 // styles
 import "./ShopContainer.scss";
 
+// components
+import { Prod } from "../Prod/Prod";
+
 export const ShopContainer = () => {
-  return <div className="shopContainer">ShopContainer</div>;
+  return (
+    <div className="shopContainer">
+      <Prod />
+    </div>
+  );
 };
