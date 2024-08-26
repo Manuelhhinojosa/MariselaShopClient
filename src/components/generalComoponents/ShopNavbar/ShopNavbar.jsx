be57e4ae7bb6d7c4b1fefc4b0ceda7a091c8990e
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2024-08-26 10:32:54 -0400

    created state using useState React hook inside of Shop component. This state provides the logic the Shop compnent needs so it can render the requested products acording to their clicks. This state is only for productions purposes. after completing the client side of this project, useState will be replaced by Redux for state handling

--- src/components/generalComoponents/ShopNavbar/ShopNavbar.jsx
+++ src/components/generalComoponents/ShopNavbar/ShopNavbar.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+// state hook for production
+import { useState } from "react";
 
 // styles
 import "./ShopNavbar.scss";
 
 export const ShopNavbar = () => {
+  // production state
+  const [isItJ, setIsItJ] = useState(true);
+
   return (
     <div className="shopNavbarContainer">
-      <div>Jewellery</div>
-      <div>Prints</div>
+      <div onClick={() => setIsItJ(true)}>Jewellery</div>
+      <div onClick={() => setIsItJ(false)}>Prints</div>
     </div>
   );
 };
