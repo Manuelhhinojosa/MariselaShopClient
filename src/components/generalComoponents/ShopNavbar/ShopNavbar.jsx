0d1d9f9beee5e80b4c389045f4e955b2538c73ae
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2024-08-26 10:19:12 -0400

    re buildt Shop component as per client's request.

--- src/components/generalComoponents/ShopNavbar/ShopNavbar.jsx
+++ src/components/generalComoponents/ShopNavbar/ShopNavbar.jsx
@@ -3,23 +3,11 @@ import React from "react";
 // styles
 import "./ShopNavbar.scss";
 
-// react router
-import { Link } from "react-router-dom";
-
 export const ShopNavbar = () => {
   return (
     <div className="shopNavbarContainer">
-      <div>
-        <Link className="shop-links" to="/shop">
-          Jewellery
-        </Link>
-      </div>
-
-      <div>
-        <Link className="shop-links" to="/shop2">
-          Prints
-        </Link>
-      </div>
+      <div>Jewellery</div>
+      <div>Prints</div>
     </div>
   );
 };
