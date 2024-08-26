0bdc4e049934fa7631411eb0d3755187d9e48f5b
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2024-08-26 11:18:52 -0400

    added mock data for products which will be used to rednder prducts

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -2,8 +2,35 @@ const homeImgUrl =
   "https://res.cloudinary.com/mangud/image/upload/v1724348584/mariselaShopClient/Screen_Shot_2024-08-22_at_1.42.14_PM_d1v3tj.png";
 
 const homeImgUrl2 =
-  "https://res.cloudinary.com/mangud/image/upload/v1724357164/mariselaShopClient/Screen_Shot_2024-08-22_at_4.05.55_PM_uc965v.png";
+  "https://res.cloudinary.com/mangud/image/upload/v1724357016/mariselaShopClient/Screen_Shot_2024-08-22_at_4.03.27_PM_j7t4lo.png";
 
+const jArr = [
+  {
+    id: 1,
+    available: true,
+    title: "Lorem ipsum dolor",
+    shortDesc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+    longDesc:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam porro alias debitis inventore eius reprehenderit ratione similique, nulla consectetur iste velit, ipsum impedit, dolores est tenetur pariatur deserunt vel ad. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam porro alias debitis inventore eius reprehenderit ratione similique, nulla consectetur iste velit, ipsum impedit, dolores est tenetur pariatur deserunt vel ad. Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+    cost: "$100.00",
+    img: "https://res.cloudinary.com/mangud/image/upload/v1724613457/mariselaShopClient/Screen_Shot_2024-08-25_at_3.17.27_PM_xnxhbs.png",
+    img2: "https://res.cloudinary.com/mangud/image/upload/v1724684658/mariselaShopClient/Screen_Shot_2024-08-26_at_11.03.23_AM_xiisrh.png",
+  },
+];
+
+const printsArr = [
+  {
+    id: 1,
+    available: true,
+    title: "Lorem ipsum dolor",
+    shortDesc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+    longDesc:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam porro alias debitis inventore eius reprehenderit ratione similique, nulla consectetur iste velit, ipsum impedit, dolores est tenetur pariatur deserunt vel ad. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam porro alias debitis inventore eius reprehenderit ratione similique, nulla consectetur iste velit, ipsum impedit, dolores est tenetur pariatur deserunt vel ad. Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+    cost: "$100.00",
+    img: "https://res.cloudinary.com/mangud/image/upload/v1724684662/mariselaShopClient/Screen_Shot_2024-08-26_at_11.03.14_AM_cnk411.png",
+    img2: "https://res.cloudinary.com/mangud/image/upload/v1724685369/mariselaShopClient/Screen_Shot_2024-08-26_at_11.15.58_AM_enynii.png",
+  },
+];
 const aboutImgUrl =
   "https://res.cloudinary.com/mangud/image/upload/v1724355625/mariselaShopClient/Screen_Shot_2024-08-22_at_3.40.17_PM_iu57cr.png";
 
@@ -92,6 +119,8 @@ const reviews = [
 export default {
   homeImgUrl,
   homeImgUrl2,
+  jArr,
+  printsArr,
   aboutImgUrl,
   aboutPageText,
   questionsAndAnswers,
