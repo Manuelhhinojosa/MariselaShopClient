e20c7425a2759e693eaff72a79a2c835bb653596
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2024-09-01 13:39:05 -0400

    changed styles of Home component to meet new client's requirements

--- src/components/pageComponents/Home/Home.jsx
+++ src/components/pageComponents/Home/Home.jsx
@@ -6,10 +6,12 @@ import "./Home.scss";
 // images
 import homeImgUrl from "../../../assets/data/data.js";
 import homeImgUrl2 from "../../../assets/data/data.js";
+import data from "../../../assets/data/data.js";
 
 export const Home = () => {
   return (
     <div className="homePageContainer">
+      <div className="test">RIGA</div>
       <div>
         <div>
           <img src={homeImgUrl2.homeImgUrl2} alt="Home-Image" />
