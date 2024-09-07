d977abdeb74493009c9a1db5857eb5addbf12993
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2024-09-07 16:16:09 -0400

    changed styles in shopMenu component displaying flex column when screen width > 600 < 1000 px

--- src/components/pageComponents/ShopMenuComponent/ShopMenuComponent.jsx
+++ src/components/pageComponents/ShopMenuComponent/ShopMenuComponent.jsx
@@ -20,7 +20,7 @@ export const ShopMenuComponent = () => {
         >
           prints
         </Link>
-        <p>***</p>
+
         <Link
           to="/shopproducts"
           onClick={() => (data.isItJ = true)}
